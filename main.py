b9a91d5ac9434fc212afe73cd8e47ab58d40ca82
GladsonT <gladson.thomas.official@gmail.com>
2026-01-04 14:48:27 +0530

    v1.0

--- main.py
+++ main.py
@@ -67,68 +67,61 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory=resource_path("static")), name="static")
 templates = Jinja2Templates(directory=resource_path("templates"))
 
+def get_table_data():
+    conn = sqlite3.connect("sql_visualizer.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    tables = cursor.fetchall()
+    table_data = {}
+    for table in tables:
+        table_name = table[0]
+        cursor.execute(f"PRAGMA table_info('{table_name}');")
+        columns = [row[1] for row in cursor.fetchall()]
+        cursor.execute(f"SELECT * FROM '{table_name}';")
+        rows = cursor.fetchall()
+        table_data[table_name] = {
+            "columns": columns,
+            "rows": rows
+        }
+    conn.close()
+    return table_data
+
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    table_data = get_table_data()
+    return templates.TemplateResponse("index.html", {"request": request, "table_data": table_data})
 
 @app.post("/execute-sql", response_class=HTMLResponse)
 async def execute_sql(request: Request, sql_query: str = Form(...)):
     output = ""
-    table_data = {}
     try:
         conn = sqlite3.connect("sql_visualizer.db")
         cursor = conn.cursor()
-
+        print(sql_query)
+        _statements = []
+        # if ';' in sql_query:
+        #     _statements = sql_query.split(';')
+        
+        # for i in _statements:
         parsed = sqlparse.parse(sql_query)
         for statement in parsed:
             if statement.get_type() == 'SELECT':
                 cursor.execute(str(statement))
                 columns = [description[0] for description in cursor.description]
                 rows = cursor.fetchall()
-                output = templates.get_template("output.html").render({"request": request, "columns": columns, "rows": rows})
+                output += templates.get_template("output.html").render({"request": request, "columns": columns, "rows": rows})
             else:
                 cursor.execute(str(statement))
-
-        # Fetch all tables
-        cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-        tables = cursor.fetchall()
         
-        for table in tables:
-            table_name = table[0]
-            cursor.execute(f"PRAGMA table_info('{table_name}');")
-            columns = [row[1] for row in cursor.fetchall()]
-            
-            cursor.execute(f"SELECT * FROM '{table_name}';")
-            rows = cursor.fetchall()
-            
-            table_data[table_name] = {
-                "columns": columns,
-                "rows": rows
-            }
-            
         conn.commit()
         conn.close()
 
+        table_data = get_table_data()
         tables_html = templates.get_template("tables.html").render({"request": request, "table_data": table_data})
         return HTMLResponse(content=f"{tables_html}<div id='output-section' hx-swap-oob='true'>{output}</div>")
 
     except Exception as e:
-        conn = sqlite3.connect("sql_visualizer.db")
-        cursor = conn.cursor()
-        cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-        tables = cursor.fetchall()
-        table_data = {}
-        for table in tables:
-            table_name = table[0]
-            cursor.execute(f"PRAGMA table_info('{table_name}');")
-            columns = [row[1] for row in cursor.fetchall()]
-            cursor.execute(f"SELECT * FROM '{table_name}';")
-            rows = cursor.fetchall()
-            table_data[table_name] = {
-                "columns": columns,
-                "rows": rows
-            }
-        conn.close()
+        table_data = get_table_data()
         tables_html = templates.get_template("tables.html").render({"request": request, "table_data": table_data})
         output = templates.get_template("output.html").render({"request": request, "error": str(e)})
         return HTMLResponse(content=f"{tables_html}<div id='output-section' hx-swap-oob='true'>{output}</div>")
